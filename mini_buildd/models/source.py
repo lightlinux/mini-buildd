7822017ec49226d902b0caf0ea3688a493659f8c
Stephan Sürken <absurd@olurdix.de>
2015-10-30 22:42:35 +0000

    [wizards] Add Ubuntu 15.10 (wily).

--- mini_buildd/models/source.py
+++ mini_buildd/models/source.py
@@ -354,6 +354,11 @@ codeversion is only used for base sources.""")
             cls._mbd_get_or_create(msglog, "Ubuntu", "vivid-backports",
                                    ["40976EAF437D05B5", "3B4FE6ACC0B21F32"],
                                    "Codename: vivid\nSuite: vivid-backports")
+            cls._mbd_get_or_create(msglog, "Ubuntu", "wily",
+                                   ["40976EAF437D05B5", "3B4FE6ACC0B21F32"])
+            cls._mbd_get_or_create(msglog, "Ubuntu", "wily-backports",
+                                   ["40976EAF437D05B5", "3B4FE6ACC0B21F32"],
+                                   "Codename: wily\nSuite: wily-backports")
 
         @classmethod
         def mbd_filter_active_base_sources(cls):
