b4d2eef06918febe5b77e1a11157699411c119d3
Stephan Sürken <absurd@olurdix.de>
2016-03-11 20:58:59 +0100

    api/_contrib.py: Improve DebianPackageTracker class.

--- mini_buildd/api/_contrib.py
+++ mini_buildd/api/_contrib.py
@@ -6,19 +6,23 @@ import urllib2
 import bs4
 
 class DebianPackageTracker():
-    _URL = "https://tracker.debian.org"
-    def __init__(self, src_package):
-        url = "{b}/pkg/{p}".format(b=self._URL, p=src_package)
-        self._page = urllib2.urlopen(url).read()
-        self.versions = {}
+    def __init__(self, src_package, tracker_url="https://tracker.debian.org"):
+        self.info = {}
 
-        soup = bs4.BeautifulSoup(self._page)
+        pkg_url = "{b}/pkg/{p}".format(b=tracker_url, p=src_package)
+        soup = bs4.BeautifulSoup(urllib2.urlopen(pkg_url).read())
         version_tags = soup.findAll("span", { "class" : "versions-repository" })
         for d in version_tags:
             codename = d['title'].split("(")[1].split(" ")[0].translate({ord(")"): None})
             version = d.find_next_sibling("a").contents[0]
-            self.versions[codename] = {"version": version,
-                                       "changelog_url": "{b}/media/packages/{h}/{p}/changelog-{v}".format(b=self._URL,
-                                                                                                          h=src_package[:4] if src_package.startswith("lib") else src_package[0],
-                                                                                                          p=src_package,
-                                                                                                          v=version)}
+            self.info[codename] = {"version": version,
+                                   "changelog_url": "{b}/media/packages/{h}/{p}/changelog-{v}".format(b=tracker_url,
+                                                                                                      h=src_package[:4] if src_package.startswith("lib") else src_package[0],
+                                                                                                      p=src_package,
+                                                                                                      v=version)}
+
+    def getInfo(self, codename):
+        return self.info[codename]
+
+    def getVersion(self, codename):
+        return self.info[codename]["version"]
