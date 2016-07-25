08aa4d56b58cfc5981a03aefe18b311682aa0645
Stephan Sürken <absurd@olurdix.de>
2016-07-25 16:04:17 +0000

    mini_buildd/[root_]urls.py: Update from deprecated syntax (tested to be compatible with django 1.7,1.8,1.9 and 1.10).

--- mini_buildd/root_urls.py
+++ mini_buildd/root_urls.py
@@ -4,21 +4,20 @@ from __future__ import unicode_literals
 import django.views.generic.base
 import django.contrib.admin
 import django.contrib.auth.views
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 django.contrib.admin.autodiscover()
 
-urlpatterns = patterns(
-    "",
+urlpatterns = [
     # mini_buildd
-    (r"^$", django.views.generic.base.RedirectView.as_view(url="/mini_buildd/", permanent=False)),
-    (r"^mini_buildd/", include("mini_buildd.urls")),
+    url(r"^$", django.views.generic.base.RedirectView.as_view(url="/mini_buildd/", permanent=False)),
+    url(r"^mini_buildd/", include("mini_buildd.urls")),
     # admin
-    (r"^admin/doc/", include("django.contrib.admindocs.urls")),
-    (r"^admin/", include(django.contrib.admin.site.urls)),
+    url(r"^admin/doc/", include("django.contrib.admindocs.urls")),
+    url(r"^admin/", include(django.contrib.admin.site.urls)),
     # registration
     url(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', django.contrib.auth.views.password_reset_confirm, name='auth_password_reset_confirm'),
-    (r'^accounts/', include("registration.backends.default.urls")),
+    url(r'^accounts/', include("registration.backends.default.urls")),
     # registration: This extra line is needed for p-d-registration since some django update...
-    (r'^accounts/', include('django.contrib.auth.urls')),
-)
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+]
--- mini_buildd/urls.py
+++ mini_buildd/urls.py
@@ -2,18 +2,18 @@
 from __future__ import unicode_literals
 
 import django.views.generic.detail
-from django.conf.urls import patterns
+from django.conf.urls import url
 
 import mini_buildd.views
 import mini_buildd.models.repository
 
-urlpatterns = patterns(
-    '',
-    (r"^$", mini_buildd.views.home),
-    (r"^log/(.+)/(.+)/(.+)/$", mini_buildd.views.log),
-    (r"^repositories/(?P<pk>.+)/$", django.views.generic.detail.DetailView.as_view(model=mini_buildd.models.repository.Repository)),
-    (r"^api$", mini_buildd.views.api),
-    (r"^accounts/profile/$", mini_buildd.views.AccountProfileView.as_view(template_name="mini_buildd/account_profile.html")),)
+urlpatterns = [
+    url(r"^$", mini_buildd.views.home),
+    url(r"^log/(.+)/(.+)/(.+)/$", mini_buildd.views.log),
+    url(r"^repositories/(?P<pk>.+)/$", django.views.generic.detail.DetailView.as_view(model=mini_buildd.models.repository.Repository)),
+    url(r"^api$", mini_buildd.views.api),
+    url(r"^accounts/profile/$", mini_buildd.views.AccountProfileView.as_view(template_name="mini_buildd/account_profile.html")),
+]
 
 django.conf.urls.handler400 = mini_buildd.views.error400_bad_request
 django.conf.urls.handler401 = mini_buildd.views.error401_unauthorized
