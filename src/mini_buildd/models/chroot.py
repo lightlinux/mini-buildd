11fd16f67025d859852208058b71fa4193a0b923
Stephan Sürken <absurd@olurdix.de>
2017-03-30 11:59:14 +0000

    models/chroot.py: Use dict.get() with default value instead of try/catch for personality value (obsoletes pylint exception).

--- src/mini_buildd/models/chroot.py
+++ src/mini_buildd/models/chroot.py
@@ -179,10 +179,7 @@ chroots (with <kbd>qemu-user-static</kbd> installed).
         if self.personality_override:
             self.personality = self.personality_override  # pylint: disable=redefined-variable-type
         else:
-            try:
-                self.personality = self.PERSONALITIES[self.architecture.name]
-            except:  # pylint: disable=bare-except
-                self.personality = "linux"
+            self.personality = self.PERSONALITIES.get(self.architecture.name, "linux")
 
         mini_buildd.misc.ConfFile(
             self.mbd_get_schroot_conf_file(),
