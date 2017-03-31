2d378bee1dfb3271b06f7befbc3d7f63122d2402
Stephan Sürken <absurd@olurdix.de>
2017-03-31 07:14:22 +0000

    models/chroot.py: Simplify/improve code to set personality (obsoletes pylint exception redefined-variable-type).

--- src/mini_buildd/models/chroot.py
+++ src/mini_buildd/models/chroot.py
@@ -175,11 +175,7 @@ chroots (with <kbd>qemu-user-static</kbd> installed).
         os.makedirs(self.mbd_get_path(), exist_ok=True)
 
         # Set personality
-        self.personality = ""
-        if self.personality_override:
-            self.personality = self.personality_override  # pylint: disable=redefined-variable-type
-        else:
-            self.personality = self.PERSONALITIES.get(self.architecture.name, "linux")
+        self.personality = self.personality_override if self.personality_override else self.PERSONALITIES.get(self.architecture.name, "linux")
 
         mini_buildd.misc.ConfFile(
             self.mbd_get_schroot_conf_file(),
