4aa5020e605aed719ba222e237076e058452a9c2
Stephan Sürken <absurd@olurdix.de>
2018-03-06 12:10:48 +0100

    django_settings.py: Add request context.

--- src/mini_buildd/django_settings.py
+++ src/mini_buildd/django_settings.py
@@ -156,6 +156,7 @@ def configure(smtp_string, loglevel):
                         # former django default (>= 1.9, the default is empty):
                         "django.contrib.auth.context_processors.auth",
                         "django.template.context_processors.debug",
+                        "django.template.context_processors.request",
                         "django.template.context_processors.i18n",
                         "django.template.context_processors.media",
                         "django.template.context_processors.static",
