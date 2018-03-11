c8acb081cadcd2fbcc8f36e420e80433965fe138
Stephan Sürken <absurd@olurdix.de>
2018-03-11 13:32:55 +0100

    home.html: Replace "js toggle" by "html5 details".
    This removes all "js toggle", and thusly also removes the
    resp, JS code, the previously used show/hide images and some
    css classes.

--- src/mini_buildd/static/js/mini-buildd.js
+++ src/mini_buildd/static/js/mini-buildd.js
@@ -1,24 +1,3 @@
-// Toggle a element on/off
-// elName    : id of the element to toggle
-// textElName: id of the element with the toggle action text (show, hide)
-function mbdToggleElement(elName, textElName, hideText, showText)
-{
-		var el = document.getElementById(elName);
-		var textEl = document.getElementById(textElName);
-		if (el.style.display == "block")
-		{
-				// Toggle to "show"
-				textEl.innerHTML = showText
-				el.style.display = "none";
-		}
-		else
-		{
-				// Toggle to "hide"
-				textEl.innerHTML = hideText
-				el.style.display = "block";
-		}
-};
-
 // Make selection on a (multiple) select via a regex
 // selectID: id of the 'select' element
 // regexID : id of the input element with the regex in 'value'
