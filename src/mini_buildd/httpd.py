82661d1f92451cf81c4977e0445e9021525b4129
Stephan Sürken <absurd@olurdix.de>
2018-05-18 18:27:59 +0200

    httpd.py: Backends: Rather use inheritance model.

--- src/mini_buildd/httpd.py
+++ src/mini_buildd/httpd.py
@@ -1,47 +1,63 @@
 # -*- coding: utf-8 -*-
 
+import abc
 import logging
 
 import mini_buildd.misc
 import mini_buildd.setup
-import mini_buildd.httpd_cherrypy
 
 LOG = logging.getLogger(__name__)
 
 
-class HttpD(object):
-    def __init__(self, backend, bind, wsgi_app):
-        self._backend = backend
-        self._backend.setup(bind, wsgi_app)
+class HttpD(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def add_static_handler(self, path, root, with_index=False, match="", with_manual_missing_error=False):
+        "Serve static files from a directory."
+        pass
 
+    @abc.abstractmethod
+    def __init__(self, bind, wsgi_app):
+        """
+        Setup HTTP server.
+
+        :param bind: the bind address to use.
+        :type bind: string
+        :param wsgi_app: the web application to process.
+        :type wsgi_app: WSGI-application
+        """
+        pass
+
+    def __init__(self):
         # Serve mini_buildd webapp's static directory
-        self._backend.add_static_handler(mini_buildd.setup.STATIC_URL,
-                                         "{p}/mini_buildd/static".format(p=mini_buildd.setup.PY_PACKAGE_PATH))
+        self.add_static_handler(mini_buildd.setup.STATIC_URL,
+                                "{p}/mini_buildd/static".format(p=mini_buildd.setup.PY_PACKAGE_PATH))
 
         # Serve django admin webapp's static directory.
         # Note: 'STATIC_URL' has trailing "/"; cherrypy does not like double slashes inside path like add_static_handler("/my//path").
-        self._backend.add_static_handler("{p}admin/".format(p=mini_buildd.setup.STATIC_URL),
-                                         "{p}/django/contrib/admin/static/admin".format(p=mini_buildd.setup.PY_PACKAGE_PATH))
+        self.add_static_handler("{p}admin/".format(p=mini_buildd.setup.STATIC_URL),
+                                "{p}/django/contrib/admin/static/admin".format(p=mini_buildd.setup.PY_PACKAGE_PATH))
 
         # Serve mini-buildd's HTML manual
-        self._backend.add_static_handler("/doc/",
-                                         mini_buildd.setup.MANUAL_DIR,
-                                         with_manual_missing_error=True)
+        self.add_static_handler("/doc/",
+                                mini_buildd.setup.MANUAL_DIR,
+                                with_manual_missing_error=True)
 
         # Serve repositories with index support
-        self._backend.add_static_handler("/repositories/",
-                                         mini_buildd.setup.REPOSITORIES_DIR,
-                                         with_index=True,
-                                         match=r"^/.+/(pool|dists)/.*")
+        self.add_static_handler("/repositories/",
+                                mini_buildd.setup.REPOSITORIES_DIR,
+                                with_index=True,
+                                match=r"^/.+/(pool|dists)/.*")
 
         # Serve logs with index support
-        self._backend.add_static_handler("/log/",
-                                         mini_buildd.setup.LOG_DIR,
-                                         with_index=True,
-                                         match=r"^/.+/.*")
+        self.add_static_handler("/log/",
+                                mini_buildd.setup.LOG_DIR,
+                                with_index=True,
+                                match=r"^/.+/.*")
 
+    @abc.abstractmethod
     def run(self):
-        self._backend.run()
+        "Run the HTTP server. Must be implemented by backend."
+        pass
 
 
 def run(bind, wsgi_app):
@@ -54,4 +70,5 @@ def run(bind, wsgi_app):
     :type wsgi_app: WSGI-application
 
     """
-    HttpD(mini_buildd.httpd_cherrypy, bind, wsgi_app).run()
+    from mini_buildd.httpd_cherrypy import Backend
+    Backend(bind, wsgi_app).run()
--- src/mini_buildd/httpd_cherrypy.py
+++ src/mini_buildd/httpd_cherrypy.py
@@ -13,54 +13,56 @@ import cherrypy.lib.static
 
 import mini_buildd.misc
 import mini_buildd.setup
+import mini_buildd.httpd
 
 LOG = logging.getLogger(__name__)
 
 
-class StaticWithIndex(cherrypy._cptools.HandlerTool):  # pylint: disable=protected-access
-    _TABLE_HEADER = """\
+class Backend(mini_buildd.httpd.HttpD):
+    class StaticWithIndex(cherrypy._cptools.HandlerTool):  # pylint: disable=protected-access
+        _TABLE_HEADER = """\
 <tr>
  <th style="text-align: left;">Name</th>
  <th style="text-align: left; padding: 0px 15px 0px 15px">Last modified</th>
  <th style="text-align: right;">Size</th>
 </tr>"""
 
-    _TABLE_SEPARATOR = """\
+        _TABLE_SEPARATOR = """\
 <tr><th colspan="3"><hr /></th></tr>"""
 
-    _TABLE_ROW = """\
+        _TABLE_ROW = """\
 <tr>
  <td style="text-align: left;"><a href="{name}" title="{name}"><kbd>{name}</kbd></a></td>
  <td style="text-align: left; padding: 0px 15px 0px 15px"><kbd><em>{mod}</em></kbd></td>
  <td style="text-align: right;"><kbd>{size}</kbd></td>
 </tr>"""
 
-    @classmethod
-    def _mbd_html_index(cls, directory):
-        "Generate a directory index as html."
+        @classmethod
+        def _mbd_html_index(cls, directory):
+            "Generate a directory index as html."
 
-        def table_rows(directory):
-            "Return an array of strings formatted as html table rows for all directory entries."
-            result = []
+            def table_rows(directory):
+                "Return an array of strings formatted as html table rows for all directory entries."
+                result = []
 
-            def add(path, entry, as_dir):
-                entry_path = os.path.join(path, entry)
-                result.append(cls._TABLE_ROW.format(name=entry + ("/" if as_dir else ""),
-                                                    mod=email.utils.formatdate(os.path.getmtime(entry_path)),
-                                                    size="DIR" if as_dir else os.path.getsize(entry_path)))
+                def add(path, entry, as_dir):
+                    entry_path = os.path.join(path, entry)
+                    result.append(cls._TABLE_ROW.format(name=entry + ("/" if as_dir else ""),
+                                                        mod=email.utils.formatdate(os.path.getmtime(entry_path)),
+                                                        size="DIR" if as_dir else os.path.getsize(entry_path)))
 
-            # Only walk one step
-            path, dirs, files = next(os.walk(directory))
-            # Dirs first, and sort entries by name
-            for entry in sorted(dirs):
-                add(path, entry, True)
+                # Only walk one step
+                path, dirs, files = next(os.walk(directory))
+                # Dirs first, and sort entries by name
+                for entry in sorted(dirs):
+                    add(path, entry, True)
 
-            for entry in sorted(files):
-                add(path, entry, False)
+                for entry in sorted(files):
+                    add(path, entry, False)
 
-            return result
+                return result
 
-        return bytes("""\
+            return bytes("""\
 <!DOCTYPE html>
 
 <html>
@@ -87,120 +89,119 @@ class StaticWithIndex(cherrypy._cptools.HandlerTool):  # pylint: disable=protect
            mbd_version=mini_buildd.__version__,
            cp_version=cherrypy.__version__), encoding=mini_buildd.setup.CHAR_ENCODING)
 
-    @classmethod
-    def _mbd_serve_index(cls, _section, directory, root="", match="", **_kwargs):
-        if match and not re.search(match, cherrypy.request.path_info):
-            raise cherrypy.HTTPError(403, "Requested path does not match allowed regex.")
+        @classmethod
+        def _mbd_serve_index(cls, _section, directory, root="", match="", **_kwargs):
+            if match and not re.search(match, cherrypy.request.path_info):
+                raise cherrypy.HTTPError(403, "Requested path does not match allowed regex.")
 
-        # Compute absolute directory path to serve
-        path = os.path.realpath(os.path.join(root, directory, cherrypy.request.path_info.lstrip("/")))
+            # Compute absolute directory path to serve
+            path = os.path.realpath(os.path.join(root, directory, cherrypy.request.path_info.lstrip("/")))
 
-        if not path.startswith(os.path.normpath(root)):
-            raise cherrypy.HTTPError(403, "Requested path outside root directory.")
+            if not path.startswith(os.path.normpath(root)):
+                raise cherrypy.HTTPError(403, "Requested path outside root directory.")
 
-        # Check that the path actually exists
-        try:
-            path_stat = os.stat(path)
-        except OSError:
-            # This will trigger a 404
+            # Check that the path actually exists
+            try:
+                path_stat = os.stat(path)
+            except OSError:
+                # This will trigger a 404
+                return False
+
+            if stat.S_ISDIR(path_stat.st_mode):
+                cherrypy.request.is_index = True
+
+                # Set last modified, and call validate_since
+                # This may return a "304 Not Modified" in case the client did a condition GET
+                cherrypy.response.headers["Last-Modified"] = cherrypy.lib.httputil.HTTPDate(path_stat.st_mtime)
+                cherrypy.lib.cptools.validate_since()
+
+                # Check for trailing "/" (without, browser will use wrong URLs for entries)
+                # This may return a "301 Moved Permanently" with Location dir + "/"
+                cherrypy.lib.cptools.trailing_slash()
+
+                # Produce and deliver a new index
+                cherrypy.response.body = cls._mbd_html_index(path)
+                cherrypy.response.headers["Content-Type"] = "text/html"
+                return True
+
             return False
 
-        if stat.S_ISDIR(path_stat.st_mode):
-            cherrypy.request.is_index = True
+        @classmethod
+        def _mbd_serve(cls, section, directory, **kwargs):
+            "Try cherrypy static serve, fallback to . Try built-in static dir first"
+            if cherrypy.lib.static.staticdir(section, directory, **kwargs):
+                return True
+            return cls._mbd_serve_index(section, directory, **kwargs)
 
-            # Set last modified, and call validate_since
-            # This may return a "304 Not Modified" in case the client did a condition GET
-            cherrypy.response.headers["Last-Modified"] = cherrypy.lib.httputil.HTTPDate(path_stat.st_mtime)
-            cherrypy.lib.cptools.validate_since()
-
-            # Check for trailing "/" (without, browser will use wrong URLs for entries)
-            # This may return a "301 Moved Permanently" with Location dir + "/"
-            cherrypy.lib.cptools.trailing_slash()
-
-            # Produce and deliver a new index
-            cherrypy.response.body = cls._mbd_html_index(path)
-            cherrypy.response.headers["Content-Type"] = "text/html"
-            return True
-
-        return False
+        def __init__(self):
+            super().__init__(self._mbd_serve)
 
     @classmethod
-    def _mbd_serve(cls, section, directory, **kwargs):
-        "Try cherrypy static serve, fallback to . Try built-in static dir first"
-        if cherrypy.lib.static.staticdir(section, directory, **kwargs):
-            return True
-        return cls._mbd_serve_index(section, directory, **kwargs)
+    def _error_manual_missing(cls, status, message, traceback, version):  # Exact arg names needed (cherrypy calls back with named arguments)  # pylint: disable=unused-argument
+        return mini_buildd.setup.DOC_MISSING_HTML_TEMPLATE.format(status=status)
 
-    def __init__(self):
-        super().__init__(self._mbd_serve)
+    def add_static_handler(self, path, root, with_index=False, match="", with_manual_missing_error=False):
+        "Shortcut to add a static handler."
+        mime_text_plain = "text/plain; charset={charset}".format(charset=mini_buildd.setup.CHAR_ENCODING)
 
+        ht = self.StaticWithIndex() if with_index else cherrypy.tools.staticdir
 
-def _error_manual_missing(status, message, traceback, version):  # Exact arg names needed (cherrypy calls back with named arguments)  # pylint: disable=unused-argument
-    return mini_buildd.setup.DOC_MISSING_HTML_TEMPLATE.format(status=status)
+        cherrypy.tree.mount(
+            ht.handler("/",
+                       "",
+                       root=root,
+                       match=match,
+                       content_types={"log": mime_text_plain,
+                                      "buildlog": mime_text_plain,
+                                      "changes": mime_text_plain,
+                                      "dsc": mime_text_plain}),
+            path,
+            config={"/": {"error_page.default": self._error_manual_missing} if with_manual_missing_error else {}})
 
+    def __init__(self, bind, wsgi_app):
+        """
+        Construct the CherryPy WSGI Web Server.
 
-def add_static_handler(path, root, with_index=False, match="", with_manual_missing_error=False):
-    "Shortcut to add a static handler."
-    mime_text_plain = "text/plain; charset={charset}".format(charset=mini_buildd.setup.CHAR_ENCODING)
+        :param bind: the bind address to use.
+        :type bind: string
+        :param wsgi_app: the web application to process.
+        :type wsgi_app: WSGI-application
 
-    ht = StaticWithIndex() if with_index else cherrypy.tools.staticdir
+        """
 
-    cherrypy.tree.mount(
-        ht.handler("/",
-                   "",
-                   root=root,
-                   match=match,
-                   content_types={"log": mime_text_plain,
-                                  "buildlog": mime_text_plain,
-                                  "changes": mime_text_plain,
-                                  "dsc": mime_text_plain}),
-        path,
-        config={"/": {"error_page.default": _error_manual_missing} if with_manual_missing_error else {}})
+        debug = "http" in mini_buildd.setup.DEBUG
+        cherrypy.config.update({"server.socket_host": str(mini_buildd.misc.HoPo(bind).host),
+                                "server.socket_port": mini_buildd.misc.HoPo(bind).port,
+                                "engine.autoreload.on": False,
+                                "checker.on": debug,
+                                "tools.log_headers.on": debug,
+                                "request.show_tracebacks": debug,
+                                "request.show_mismatched_params": debug,
+                                "log.error_file": None,
+                                "log.access_file": None,
+                                "log.screen": debug and mini_buildd.setup.FOREGROUND})
 
+        # Redirect cherrypy's error log to mini-buildd's logging
+        cherrypy.engine.subscribe("log", lambda msg, level: LOG.log(level, "CherryPy: {m}".format(m=msg)))
 
-def setup(bind, wsgi_app):
-    """
-    Run the CherryPy WSGI Web Server.
+        # Set up a rotating file handler for cherrypy's access log
+        handler = logging.handlers.RotatingFileHandler(
+            mini_buildd.setup.ACCESS_LOG_FILE,
+            maxBytes=5000000,
+            backupCount=9,
+            encoding="UTF-8")
+        handler.setLevel(logging.DEBUG)
+        handler.setFormatter(cherrypy._cplogging.logfmt)  # pylint: disable=protected-access
+        cherrypy.log.access_log.addHandler(handler)
 
-    :param bind: the bind address to use.
-    :type bind: string
-    :param wsgi_app: the web application to process.
-    :type wsgi_app: WSGI-application
+        # Register wsgi app (django)
+        cherrypy.tree.graft(wsgi_app)
 
-    """
+        super().__init__()
 
-    debug = "http" in mini_buildd.setup.DEBUG
-    cherrypy.config.update({"server.socket_host": str(mini_buildd.misc.HoPo(bind).host),
-                            "server.socket_port": mini_buildd.misc.HoPo(bind).port,
-                            "engine.autoreload.on": False,
-                            "checker.on": debug,
-                            "tools.log_headers.on": debug,
-                            "request.show_tracebacks": debug,
-                            "request.show_mismatched_params": debug,
-                            "log.error_file": None,
-                            "log.access_file": None,
-                            "log.screen": debug and mini_buildd.setup.FOREGROUND})
-
-    # Redirect cherrypy's error log to mini-buildd's logging
-    cherrypy.engine.subscribe("log", lambda msg, level: LOG.log(level, "CherryPy: {m}".format(m=msg)))
-
-    # Set up a rotating file handler for cherrypy's access log
-    handler = logging.handlers.RotatingFileHandler(
-        mini_buildd.setup.ACCESS_LOG_FILE,
-        maxBytes=5000000,
-        backupCount=9,
-        encoding="UTF-8")
-    handler.setLevel(logging.DEBUG)
-    handler.setFormatter(cherrypy._cplogging.logfmt)  # pylint: disable=protected-access
-    cherrypy.log.access_log.addHandler(handler)
-
-    # Register wsgi app (django)
-    cherrypy.tree.graft(wsgi_app)
-
-
-def run():
-    """
-    Run the CherryPy WSGI Web Server.
-    """
-    cherrypy.engine.start()
-    cherrypy.engine.block()
+    def run(self):
+        """
+        Run the CherryPy WSGI Web Server.
+        """
+        cherrypy.engine.start()
+        cherrypy.engine.block()
