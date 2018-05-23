9662ac1559c63e5e3e0e61b4aefe63557e3668ac
Stephan Sürken <absurd@olurdix.de>
2018-05-23 14:42:52 +0200

    httpd.py: Move manual index generator code from cherrypy backend to base class.

--- src/mini_buildd/httpd.py
+++ src/mini_buildd/httpd.py
@@ -1,12 +1,85 @@
 # -*- coding: utf-8 -*-
 
 import abc
+import os
+import email
 import logging
 
 import mini_buildd.misc
 import mini_buildd.setup
 
 LOG = logging.getLogger(__name__)
+
+
+class HtmlIndex(object):
+    _TABLE_HEADER = """\
+<tr>
+ <th style="text-align: left;">Name</th>
+ <th style="text-align: left; padding: 0px 15px 0px 15px">Last modified</th>
+ <th style="text-align: right;">Size</th>
+</tr>"""
+
+    _TABLE_SEPARATOR = """\
+<tr><th colspan="3"><hr /></th></tr>"""
+
+    _TABLE_ROW = """\
+<tr>
+ <td style="text-align: left;"><a href="{name}" title="{name}"><kbd>{name}</kbd></a></td>
+ <td style="text-align: left; padding: 0px 15px 0px 15px"><kbd><em>{mod}</em></kbd></td>
+ <td style="text-align: right;"><kbd>{size}</kbd></td>
+</tr>"""
+
+    @classmethod
+    def html_index(cls, directory, path_info, backend_info):
+        "Generate a directory index as html."
+
+        def table_rows(directory):
+            "Return an array of strings formatted as html table rows for all directory entries."
+            result = []
+
+            def add(path, entry, as_dir):
+                entry_path = os.path.join(path, entry)
+                result.append(cls._TABLE_ROW.format(name=entry + ("/" if as_dir else ""),
+                                                    mod=email.utils.formatdate(os.path.getmtime(entry_path)),
+                                                    size="DIR" if as_dir else os.path.getsize(entry_path)))
+
+            # Only walk one step
+            path, dirs, files = next(os.walk(directory))
+            # Dirs first, and sort entries by name
+            for entry in sorted(dirs):
+                add(path, entry, True)
+
+            for entry in sorted(files):
+                add(path, entry, False)
+
+            return result
+
+        return bytes("""\
+<!DOCTYPE html>
+
+<html>
+ <head>
+        <title>Index of {path_info}</title>
+ </head>
+ <body>
+        <h1>Index of {path_info}</h1>
+  <table>
+   {table_header}
+   {table_separator}
+   {table_parent}
+   {table_rows}
+   {table_separator}
+  </table>
+ <address>mini-buildd {mbd_version} ({backend_info})</address>
+ </body>
+</html>
+""".format(path_info=path_info,
+           table_header=cls._TABLE_HEADER,
+           table_separator=cls._TABLE_SEPARATOR,
+           table_parent=cls._TABLE_ROW.format(name="../", mod="&nbsp;", size="PARENT"),
+           table_rows="\n".join(table_rows(directory.rstrip(r"\/"))),
+           mbd_version=mini_buildd.__version__,
+           backend_info=backend_info), encoding=mini_buildd.setup.CHAR_ENCODING)
 
 
 class HttpD(metaclass=abc.ABCMeta):
--- src/mini_buildd/httpd_cherrypy.py
+++ src/mini_buildd/httpd_cherrypy.py
@@ -3,7 +3,6 @@
 import os
 import re
 import stat
-import email.utils
 import logging
 
 import cherrypy
@@ -20,75 +19,6 @@ LOG = logging.getLogger(__name__)
 
 class Backend(mini_buildd.httpd.HttpD):
     class StaticWithIndex(cherrypy._cptools.HandlerTool):  # pylint: disable=protected-access
-        _TABLE_HEADER = """\
-<tr>
- <th style="text-align: left;">Name</th>
- <th style="text-align: left; padding: 0px 15px 0px 15px">Last modified</th>
- <th style="text-align: right;">Size</th>
-</tr>"""
-
-        _TABLE_SEPARATOR = """\
-<tr><th colspan="3"><hr /></th></tr>"""
-
-        _TABLE_ROW = """\
-<tr>
- <td style="text-align: left;"><a href="{name}" title="{name}"><kbd>{name}</kbd></a></td>
- <td style="text-align: left; padding: 0px 15px 0px 15px"><kbd><em>{mod}</em></kbd></td>
- <td style="text-align: right;"><kbd>{size}</kbd></td>
-</tr>"""
-
-        @classmethod
-        def _mbd_html_index(cls, directory):
-            "Generate a directory index as html."
-
-            def table_rows(directory):
-                "Return an array of strings formatted as html table rows for all directory entries."
-                result = []
-
-                def add(path, entry, as_dir):
-                    entry_path = os.path.join(path, entry)
-                    result.append(cls._TABLE_ROW.format(name=entry + ("/" if as_dir else ""),
-                                                        mod=email.utils.formatdate(os.path.getmtime(entry_path)),
-                                                        size="DIR" if as_dir else os.path.getsize(entry_path)))
-
-                # Only walk one step
-                path, dirs, files = next(os.walk(directory))
-                # Dirs first, and sort entries by name
-                for entry in sorted(dirs):
-                    add(path, entry, True)
-
-                for entry in sorted(files):
-                    add(path, entry, False)
-
-                return result
-
-            return bytes("""\
-<!DOCTYPE html>
-
-<html>
- <head>
-  <title>Index of {path}</title>
- </head>
- <body>
-  <h1>Index of {path}</h1>
-  <table>
-   {table_header}
-   {table_separator}
-   {table_parent}
-   {table_rows}
-   {table_separator}
-  </table>
- <address>mini-buildd {mbd_version} (CherryPy {cp_version})</address>
- </body>
-</html>
-""".format(path=cherrypy.request.path_info,
-           table_header=cls._TABLE_HEADER,
-           table_separator=cls._TABLE_SEPARATOR,
-           table_parent=cls._TABLE_ROW.format(name="../", mod="&nbsp;", size="PARENT"),
-           table_rows="\n".join(table_rows(directory.rstrip(r"\/"))),
-           mbd_version=mini_buildd.__version__,
-           cp_version=cherrypy.__version__), encoding=mini_buildd.setup.CHAR_ENCODING)
-
         @classmethod
         def _mbd_serve_index(cls, _section, directory, root="", match="", **_kwargs):
             if match and not re.search(match, cherrypy.request.path_info):
@@ -120,7 +50,7 @@ class Backend(mini_buildd.httpd.HttpD):
                 cherrypy.lib.cptools.trailing_slash()
 
                 # Produce and deliver a new index
-                cherrypy.response.body = cls._mbd_html_index(path)
+                cherrypy.response.body = mini_buildd.httpd.HtmlIndex.html_index(path, cherrypy.request.path_info, "CherryPy {cp_version}".format(cp_version=cherrypy.__version__))
                 cherrypy.response.headers["Content-Type"] = "text/html"
                 return True
 
