e2595b31d8e6a34182218863bae8718ab16dca1c
Stephan Sürken <absurd@olurdix.de>
2018-07-31 16:47:17 +0200

    api/_contrib.py, DebianPackageTracker helper: Fix parsing for backport versions (after changes to tracker web sites).
    (cherry picked from commit 222b3bbfeff50d5572a0a7cce8ff8a31c9f315ac)

--- src/mini_buildd/api/_contrib.py
+++ src/mini_buildd/api/_contrib.py
@@ -26,7 +26,7 @@ class DebianPackageTracker(object):
         version_tags = soup.findAll("span", {"class": "versions-repository"})
         for d in version_tags:
             codename = d['title'].split("(")[1].split(" ")[0].translate({ord(")"): None})
-            version = d.find_next_sibling("a").contents[0]
+            version = d.find_next_sibling("a").get_text("", strip=True)
             self.info[codename] = {"version": version,
                                    "changelog_url": "{b}/media/packages/{h}/{p}/changelog-{v}".format(b=tracker_url,
                                                                                                       h=src_package[:4] if src_package.startswith("lib") else src_package[0],
