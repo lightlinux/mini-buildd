74189c3c3a07bec25e91043f00bd9c96c62fdc83
Stephan Sürken <absurd@olurdix.de>
2019-03-23 20:48:56 +0100

    httpd_twisted.py: Implement (rotating) 'access.log logging'.

--- src/mini_buildd/httpd.py
+++ src/mini_buildd/httpd.py
@@ -25,7 +25,15 @@ Package <b><tt>mini-buildd-doc</tt></b> is not installed on this site (might be 
     def __init__(self, supported_types):
         self._debug = "http" in mini_buildd.setup.DEBUG
         self._foreground = mini_buildd.setup.FOREGROUND
-        self._access_log_file = mini_buildd.setup.ACCESS_LOG_FILE
+
+        # access.log
+        self._access_log = logging.Logger(name="mini_buildd_httpd_access")   # Use a new root logger outside our hierarchy
+        self._access_log.addHandler(logging.handlers.RotatingFileHandler(
+            mini_buildd.setup.ACCESS_LOG_FILE,
+            maxBytes=5000000,
+            backupCount=9,
+            encoding="UTF-8"))
+
         self._char_encoding = mini_buildd.setup.CHAR_ENCODING
         self._mime_text_plain = "text/plain; charset={charset}".format(charset=self._char_encoding)
         self._mime_types = {"log": self._mime_text_plain,
--- src/mini_buildd/httpd_twisted.py
+++ src/mini_buildd/httpd_twisted.py
@@ -16,6 +16,24 @@ import mini_buildd.httpd
 LOG = logging.getLogger(__name__)
 
 
+class Site(twisted.web.server.Site):
+    """Twisted Suite allowing access log via python logger.
+
+    twisted Site class already allows to give a path for the access
+    log, but no means to rotate. Essentially, this class merely exists
+    to be able to make use of python's RotatingFileHandler (see
+    httpd.py).
+    """
+    def __init__(self, access_log, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._access_log = access_log
+        self._access_log.setLevel(logging.INFO)
+
+    def log(self, request):
+        line = self._logFormatter(self._logDateTime, request)  # This is the original line used in twisted Site class.
+        self._access_log.info(line)
+
+
 class RootResource(twisted.web.resource.Resource):
     """
     Twisted root resource needed to mix static and wsgi resources.
@@ -72,7 +90,7 @@ class HttpD(mini_buildd.httpd.HttpD):
 
         # HTTP setup
         self.resource = RootResource(twisted.web.wsgi.WSGIResource(twisted.internet.reactor, twisted.internet.reactor.getThreadPool(), wsgi_app))  # pylint: disable=no-member
-        self.site = twisted.web.server.Site(self.resource)
+        self.site = Site(self._access_log, self.resource)
 
         for ep in self._endpoints:
             twisted.internet.endpoints.serverFromString(twisted.internet.reactor, ep.desc).listen(self.site)
