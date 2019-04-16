88cdab1d97c20bdc96fe3908f25ddb7a4111f88e
Stephan Sürken <absurd@olurdix.de>
2019-04-16 19:38:11 +0200

    source wizards: Adapt for new signing keys for buster/updates (security).

--- src/mini_buildd/models/source.py
+++ src/mini_buildd/models/source.py
@@ -376,6 +376,7 @@ codeversion is only used for base sources.""")
                 "archive_jessie": "7638D0442B90D010",            # Debian Archive Automatic Signing Key (8/jessie) <ftpmaster@debian.org>
                 "release_jessie": "CBF8D6FD518E17E1",            # Jessie Stable Release Key <debian-release@lists.debian.org>
                 "security_archive_jessie": "9D6D8F6BC857C906",   # Debian Security Archive Automatic Signing Key (8/jessie) <ftpmaster@debian.org>
+                "security_archive_stretch": "EDA0D2388AE22BA9",  # Debian Security Archive Automatic Signing Key (9/stretch) <ftpmaster@debian.org>
                 "archive_wheezy": "8B48AD6246925553",            # Debian Archive Automatic Signing Key (7.0/wheezy) <ftpmaster@debian.org>
                 "release_wheezy": "6FB2A1C265FFB764",            # Wheezy Stable Release Key <debian-release@lists.debian.org>
             }
@@ -413,7 +414,7 @@ codeversion is only used for base sources.""")
             cls._mbd_get_or_create(msglog, "Debian", "buster",
                                    [keys["archive_wheezy"], keys["archive_jessie"], keys["archive_stretch"]])
             cls._mbd_get_or_create(msglog, "Debian", "buster/updates",
-                                   [keys["archive_wheezy"], keys["security_archive_jessie"]],
+                                   [keys["security_archive_jessie"], keys["security_archive_stretch"]],
                                    extra_options="Codename: buster\nLabel: Debian-Security\nX-Remove-From-Component: updates/")
 
             cls._mbd_get_or_create(msglog, "Debian", "sid",
