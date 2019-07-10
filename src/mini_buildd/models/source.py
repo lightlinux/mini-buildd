c8d7d5aedbf1f979951241930a22043042e96276
Stephan Sürken <absurd@olurdix.de>
2019-07-10 13:20:07 +0200

    wizards (updates after buster release): Add stretch-backports-sloppy.

--- src/mini_buildd/models/source.py
+++ src/mini_buildd/models/source.py
@@ -413,6 +413,8 @@ codeversion is only used for base sources.""")
                                    extra_options="Codename: stretch\nLabel: Debian-Security\nX-Remove-From-Component: updates/")
             cls._mbd_get_or_create(msglog, "Debian Backports", "stretch-backports",
                                    [keys["archive_jessie"], keys["archive_stretch"]])
+            cls._mbd_get_or_create(msglog, "Debian Backports", "stretch-backports-sloppy",
+                                   [keys["archive_jessie"], keys["archive_stretch"]])
 
             cls._mbd_get_or_create(msglog, "Debian", "buster",
                                    [keys["release_stretch"], keys["release_buster"], keys["archive_stretch"], keys["archive_buster"]])
